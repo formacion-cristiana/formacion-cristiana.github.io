d85c9955af22914a1fa2d0ce10bfbda9ab7192f4
aragonl <aragon_l@hotmail.com>
2025-09-11 14:28:29 -0300

    arregle tip en true-false questions

--- src/utils/normalizeQuestions.js
+++ src/utils/normalizeQuestions.js
@@ -1,16 +1,17 @@
 export function normalizeQuestions(questions) {
   return questions.map((q,idx) => {
     if (q.type === "true-false") {
-      return {
-        type: "multiple-choice",
-        questionText: q.questionText,
-        num:idx,
+      const p = {
+        ...q, // copy all properties from q, object spread syntax
+        type: "multiple-choice", // override type
+        num: idx, // add/override num
         answerOptions: [
           { answerText: "Verdadero", isCorrect: q.correctAnswer === true },
           { answerText: "Falso", isCorrect: q.correctAnswer === false },
         ],
-        ...(q.image && { image: q.image }), // include image only if it exists
-        };
+      };
+
+      return p;
     }
     q.num=idx;
     return q;
