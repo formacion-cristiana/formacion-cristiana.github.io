c13152c0103809a782a1ef8fcb0ba7fdc2db7edd
formacion-cristiana <ofs.bariloche@gmail.com>
2026-09-16 16:51:12 +0200

    nueva conf sin prerender

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,11 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { vitePrerenderPlugin } from 'vite-prerender-plugin';
-import { fileURLToPath } from 'url';
-import path from 'path';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-// ⚡ IMPORTANT for GitHub Pages + HashRouter
-// https://username.github.io/your-repo-name
-//Replace `your-repo-name` with your repo's name
-//If you deploy to a custom domain, set base: "/".
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
-    vitePrerenderPlugin({
-      renderTarget: '#root', // your client mount point
-      prerenderScript: path.resolve(__dirname, 'prerender/prerender.jsx'),
-      additionalPrerenderRoutes: ['/printable', '/print-all'],
-      // Avoid Vite trying to bundle fs/path/browser-incompatible imports
-      ssr: true,
-    })
   ],
   build: {
     outDir: "dist",
