77a01334ba0acaab5583153ff1e60f70a31db442
formacion-cristiana <ofs.bariloche@gmail.com>
2026-09-16 19:31:31 +0200

    Delete src directory

--- src/utils/shuffleArray.js
+++
@@ -1,7 +0,0 @@
-//utils/shuffleArray
-export function shuffleArray(array) {
-  return array
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
-}
